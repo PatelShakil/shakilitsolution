f580c41a9e350b27c709a3edd9b68caf1b53c7dd
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-18 14:05:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import React from 'react'
 
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-
 ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
+   <React.StrictMode>
      <App />
-   </BrowserRouter>
+   </React.StrictMode>
 ))  
