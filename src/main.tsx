05a242f3dc9a6bc96a9cb1439cd72c58bb3a01cf
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-18 15:25:58 +0530

    Fix deployment issues

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <div>
             <App />
-            <h1>This is Shakil IT Solution</h1>
-        </div>
     </React.StrictMode>
 )
