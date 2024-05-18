e192f2bce442e598b3767dc86c3011a03a2fce31
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-18 13:04:37 +0530

    deploy react app

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention:'camelCase'
-    }
-  }
+  base:"/vite-deploy/"
 })
