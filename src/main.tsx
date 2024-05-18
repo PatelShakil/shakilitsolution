ca4d0197bc989a806a932981a92a09822cf3a496
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-18 23:06:32 +0530

    Deployment 1.0

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import React from 'react'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
             <App />
