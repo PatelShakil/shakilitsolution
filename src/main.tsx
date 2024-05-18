bfc3c2dd6549dde075462f1a0f2e4a7ad7daf62f
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-18 14:36:59 +0530

    Some Changes Done

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import React from 'react'
 
-ReactDOM.render((
-   <React.StrictMode>
-     <App />
-   </React.StrictMode>
-))  
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
